c8cb280840147cc2bd77d76366df831cc56ddcd6
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-01 12:24:02 +0300

    Add initial quiz questions for the game

--- quizgame.py
+++ quizgame.py
@@ -1,2 +1,14 @@
 import random
 
+questions = [
+    {
+        "question": "What is the capital of Kenya?",
+        "choices": {
+            "A": "Nairobi",
+            "B": "Mombasa",
+            "C": "Kisumu",
+            "D": "Eldoret"
+        },
+        "answer": "A"
+    },
+]
